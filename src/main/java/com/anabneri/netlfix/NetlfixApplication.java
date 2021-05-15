0ed00919ea5ec453dcb0d471e4db2db94ad05598
Ana Beatriz Neri <ananeri32@hotmail.com>
2021-05-14 21:03:33 -0300

    initial commit

---
+++ src/main/java/com/anabneri/netlfix/NetlfixApplication.java
@@ -0,0 +1,13 @@
+package com.anabneri.netlfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetlfixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetlfixApplication.class, args);
+	}
+
+}
